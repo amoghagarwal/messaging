e412150d3806fcb31617a4ab970b1860987039d4
System Administrator <root@amoghs-MacBook-Air.local>
2016-10-26 03:06:19 +0530

    unit tests

--- accept_requests/tests.py
+++ accept_requests/tests.py
@@ -5,13 +5,13 @@ import pika
 # Create your tests here.
 
 from django.test import TestCase
-from accept_requests.utility import get_redis_connection, get_status_from_redis, get_notification_channel, create_queue
+from accept_requests.utility import get_redis_connection, get_status_from_redis, get_notification_channel, create_queue, \
+    remove_key_from_redis
 from appsphere.settings import RABBITMQ_EXCHANGE as exchange_name
 
 
-class MessagingTestCase(TestCase):
-
-    def redis_is_up(self):
+class RedisTestCase(TestCase):
+    def test_redis_is_up(self):
         """
         CHECK REDIS CONNECTION
         :return:
@@ -19,7 +19,7 @@ class MessagingTestCase(TestCase):
         r = get_redis_connection(0)
         self.assertTrue(isinstance(r, redis.StrictRedis))
 
-    def get_status(self):
+    def test_key_exists_in_redis(self):
         """
         Check if key doesn't exist in Redis
         :return:
@@ -27,7 +27,22 @@ class MessagingTestCase(TestCase):
         status = get_status_from_redis("FSFSFSD-44255", 0)
         self.assertEqual(status, "Message Already Processed")
 
-    def rabbitmq_connection(self):
+    def test_remove_key_from_redis(self):
+        """
+        Check key deletion from Redis
+        :return:
+        """
+        r = get_redis_connection(0)
+        r.set("a","b")
+        remove_key_from_redis("a", 0)
+        if r.exists("a"):
+            self.assertFalse(True)
+        else:
+            self.assertTrue(True)
+
+
+class RabbitmqTestCase(TestCase):
+    def test_rabbitmq_connection(self):
         """
         Check Rabbitmq connection
         :return:
@@ -35,16 +50,19 @@ class MessagingTestCase(TestCase):
         ch = get_notification_channel()
         self.assertTrue(isinstance(ch, pika.adapters.blocking_connection.BlockingChannel))
 
-    def queue_creation(self):
+    def test_queue_creation(self):
         """
         Check Queue Creation
         :return:
         """
-        try:
-            ch = get_notification_channel()
-            create_queue(ch, exchange_name, "process_messaging", "process")
-            self.assertTrue(True)
-        except Exception as ex:
-            self.assertFalse(False)
+        ch = get_notification_channel()
+        create_queue(ch, exchange_name, "process_messaging", "process")
+        self.assertTrue(True)
 
 
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
